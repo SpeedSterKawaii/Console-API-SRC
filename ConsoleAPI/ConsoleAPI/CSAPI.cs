cf180115c9da2cdf62cde155eee730c956b4a29c
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-05-24 11:59:21 -0400

    Add files via upload

---
+++ ConsoleAPI/ConsoleAPI/CSAPI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPI
+{
+    public class CSAPI
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool C();
+
+        public void Create(string title) 
+        {
+            C();
+            Console.Title = title;
+        }
+
+        public void Line(string line) 
+        {
+            Console.WriteLine(line);
+        }
+
+        public void BGColor(ConsoleColor b) 
+        {
+            Console.BackgroundColor = b;
+        }
+
+        public void FGColor(ConsoleColor e)
+        {
+            Console.ForegroundColor = e;
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public void ReadK()
+        {
+            Console.ReadKey();
+        }
+
+        public void ReadL()
+        {
+            Console.ReadLine();
+        }
+    }
+}
